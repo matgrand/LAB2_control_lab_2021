d96a43b4f2a07e511a9424ac9ed773ad584d75af
grandin <matteo.grandin@gmail.com>
2021-04-25 15:21:32 +0200

    simplifying things a lot

---
+++ CLAB2Matteo/ass211_4_____ADD_FEEDFORWARD.m
@@ -0,0 +1,33 @@
+
+%assignment 2.1.1 (4) (OPTIONAL)
+%PID + FEEDFORWARD and simulation
+
+
+clear all;
+%load the motor params
+load_params_inertial_case();
+controldesign(); 
+
+%(1)
+%PID CONTROLLER 
+% Useful results
+ts_5 = 0.08;
+Mp = 0.1;
+
+CT_PID();
+
+%sampling time at 10 ms
+Ts = 0.01; 
+z = tf('z', Ts);
+
+% 1st test 
+% B-Euler
+s = (1-z^-1)/Ts;
+
+sysC_d = Kp+Ki/s+Kd*s/(Tl*s+1);
+sysC_d = minreal(sysC_d)
+
+[numC_d, denC_d] = tfdata(sysC_d, 'v')
+
+%run simulink
+open_system('discretePIDmodelFF');
--- CLAB2Matteo/assignments_points/ass320_1.m
+++
@@ -1,7 +0,0 @@
-%ass 3.2 (1)
-
-ass220_1_2_3_DT_SS_NOMINAL();
-
-close_system('SSDiscreteDirect');
-
-open_system('LAB2_2');
